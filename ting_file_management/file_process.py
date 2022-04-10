c010b14d1f5f9a628afe3c578a956d94676a662a
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-04-10 12:07:31 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        read_file = instance.search(index)
+        if read_file['nome_do_arquivo'] == path_file:
+            return None
+
+    news = txt_importer(path_file)
+    output = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(news),
+        'linhas_do_arquivo': news
+    }
+    instance.enqueue(output)
+    print(str(output), file=sys.stdout)
 
 
 def remove(instance):
