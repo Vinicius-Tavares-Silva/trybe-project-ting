25177588857960f99076c53f2f3126900a245a14
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-04-10 11:30:28 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith('.txt'):
+            news = read_txt_file(path_file)
+            return news
+        else:
+            print('Formato inválido', file=sys.stderr)
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+
+
+def read_txt_file(path_file):
+    file = open(path_file, 'r')
+    lines = file.readlines()
+    lines = [line.rstrip('\n') for line in lines]
+    return lines
