513acb08cce0875206722a2e2bd95261d9b0680c
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-04-10 12:40:07 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -16,4 +16,20 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    output = []
+    for index in range(len(instance)):
+        found_at = []
+        read_file = instance.search(index)
+        for index, line in enumerate(read_file['linhas_do_arquivo']):
+            if word.casefold() in line.casefold():
+                found_at.append({
+                    'linha': index + 1,
+                    'conteudo': line
+                })
+        if len(found_at) > 0:
+            output.append({
+                'palavra': word,
+                'arquivo': read_file['nome_do_arquivo'],
+                'ocorrencias': found_at
+            })
+    return output
